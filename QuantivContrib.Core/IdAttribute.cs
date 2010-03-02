972e98da2c6e7e6aba17803dd1729a13e80813c2
David Whitney <david@davidwhitney.co.uk>
2010-03-02 20:43:20 +0000

    Removed moved Attributes

--- QuantivContrib.Core/IdAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Quantiv5UsageExamples.QuantivContrib
-{
-    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-    public class IdAttribute: Attribute
-    {
-    }
-}
