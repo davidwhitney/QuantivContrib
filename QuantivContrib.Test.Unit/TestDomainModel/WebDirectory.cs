3503b876cb2ba283aa5ae3fba73640e26bc9c91e
David Whitney <david@davidwhitney.co.uk>
2010-03-03 17:44:46 +0000

    Added properties.

--- QuantivContrib.Test.Unit/TestDomainModel/WebDirectory.cs
+++ QuantivContrib.Test.Unit/TestDomainModel/WebDirectory.cs
@@ -1,3 +1,4 @@
+using System;
 using QuantivContrib.Core;
 using QuantivContrib.Core.Attributes;
 
@@ -10,7 +11,13 @@ namespace QuantivContrib.Test.Unit.TestDomainModel
         public virtual int WebDirectoryId { get; set; }
 
         [AttributeRef]
+        public virtual string WebDirectoryRef { get; set; }
+
+        [AttributeRef]
         public virtual string WebDirectoryName { get; set; }
+
+        [AttributeRef]
+        public virtual string ParentDirectoryName { get; set; }
 
         [AttributeRef]
         public virtual int DomainId { get; set; }
@@ -19,7 +26,19 @@ namespace QuantivContrib.Test.Unit.TestDomainModel
         public virtual bool IsReserved { get; set; }
 
         [AttributeRef]
+        public virtual bool? IsAvailable { get; set; }
+
+        [AttributeRef]
+        public virtual bool? IsCreated { get; set; }
+
+        [AttributeRef]
+        public virtual bool IsForOnlineReporting { get; set; }
+
+        [AttributeRef]
         public virtual int FundraiserId { get; set; }
+
+        [AttributeRef]
+        public virtual int? FundraiserRevenueStreamId { get; set; }
 
         [AttributeRef]
         public virtual int EventGivingGroupId { get; set; }
@@ -38,5 +57,11 @@ namespace QuantivContrib.Test.Unit.TestDomainModel
 
         [AttributeRef("EventGivingGroup/Status")]
         public virtual int EventGivingGroupStatus { get; set; }
+
+        [AttributeRef]
+        public virtual DateTime? DateCreated { get; set; }
+
+        [AttributeRef]
+        public virtual DateTime? DateDeleted { get; set; }
     }
 }
